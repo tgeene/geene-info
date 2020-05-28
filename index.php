6113be6d4a13320f0c17423c4fe7f1583b23d642
Trevor Geene <trevor.geene@gmail.com>
2020-05-27 21:11:58 -0700

    Initial Site Upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!-- Inspired by the geniuses behind http://motherfuckingwebsite.com and http://bettermotherfuckingwebsite.com. -->
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="author" content="Trevor Geene">
+        <meta name="description" content="Personal sites for people with the Geene Family name.">
+        <meta name="keywords" content="Geene,Family,Personal,Sites,Trevor Geene">
+		<meta name="robots" content="index, follow">
+		<meta name="revisit-after" content="1 month">
+		<title>Geene Family Directory</title>
+		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/defaults.css">
+		<link rel="stylesheet" href="css/styles.css">
+	<head>
+	<body>
+		<div id="wrapper">
+			<header id="contact">
+				<h1>Geene Family</h1>
+			</header>
+			<div id="content">
+				<section id="sites">
+                    <header>
+                        <h2>Personal Sites</h2>
+                    </header>
+                    <ul>
+                        <li>
+                            <a title="Trevor Geene's Resume" href="http://trevor.geene.info">Trevor Geene's Resume</a>
+                        </li>
+                    </ul>
+                </section>
+			</div>
+			<footer id="meta">
+				<p>Geene Family Directory &copy; <?php echo date("Y"); ?>.</p>
+			</footer>
+		</div>
+	</body>
+</html>
