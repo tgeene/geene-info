3f3cd14779c3e67b1ad85cf36d645d563245c539
Trevor <trevor.geene@gmail.com>
2020-09-21 19:49:34 -0700

    Https/CloudFlare Changes

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="author" content="Trevor Geene">
-        <meta name="description" content="Personal sites for people with the Geene Family name.">
-        <meta name="keywords" content="Geene,Family,Personal,Sites,Trevor Geene">
+    <meta name="description" content="Personal sites for people with the Geene Family name.">
+    <meta name="keywords" content="Geene,Family,Personal,Sites,Trevor Geene">
 		<meta name="robots" content="index, follow">
 		<meta name="revisit-after" content="1 month">
 		<title>Geene Family Directory</title>
 		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
-		<link rel="stylesheet" href="css/reset.css">
-		<link rel="stylesheet" href="css/defaults.css">
 		<link rel="stylesheet" href="css/styles.css">
 	<head>
 	<body>
@@ -23,15 +21,15 @@
 			</header>
 			<div id="content">
 				<section id="sites">
-                    <header>
-                        <h2>Personal Sites</h2>
-                    </header>
-                    <ul>
-                        <li>
-                            <a title="Trevor Geene's Resume" href="http://trevor.geene.co">Trevor Geene's Resume</a>
-                        </li>
-                    </ul>
-                </section>
+          <header>
+            <h2>Personal Sites</h2>
+          </header>
+          <ul>
+            <li>
+              <a title="Trevor Geene's Resume" href="https://trevor.geene.co">Trevor Geene's Resume</a>
+            </li>
+          </ul>
+        </section>
 			</div>
 			<footer id="meta">
 				<p>Geene Family Directory &copy; <?php echo date("Y"); ?>.</p>
